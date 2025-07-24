360da53b13e428d3b3a700a7b0f7582928b64bd0
tuxy <subham020304@gmail.com>
2025-07-24 18:09:53 +0530

    feat: add attendance forward route handling from firmware to mms
    - Add /:device/attendance/new route for getting frames from firmware.
    - POST firmware to mms for recognition.

--- cmd/server.go
+++ cmd/server.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 )
+
+type FrameMetadata struct {
+	DeviceID  int16
+	CreatedAt time.Time
+	Location  string
+	Status    bool
+	ErrorMsg  string
+}
 
 func GetIndexHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]any{
@@ -22,6 +35,78 @@ func GetIndexHandler(c echo.Context) error {
 func GetHealthHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
 		"status": "ok",
+	})
+}
+
+func sendFramesToMl(fileHeaders []*multipart.FileHeader) (*http.Response, error) {
+	var body bytes.Buffer
+	formWriter := multipart.NewWriter(&body)
+	for _, fileHeader := range fileHeaders {
+		src, err := fileHeader.Open()
+		if err != nil {
+			return nil, err
+		}
+		defer func() {
+			err := src.Close()
+			if err != nil {
+				log.Warn().Err(err).Msg("Warning: Failed to close file")
+			}
+		}()
+		part, err := formWriter.CreateFormFile("img", fileHeader.Filename)
+		if err != nil {
+			return nil, err
+		}
+		_, err = io.Copy(part, src)
+		if err != nil {
+			return nil, err
+		}
+	}
+	err := formWriter.Close()
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", os.Getenv("MMS_URL"), &body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", formWriter.FormDataContentType())
+
+	client := &http.Client{}
+	return client.Do(req)
+}
+
+func PostAttendanceNewHandler(c echo.Context) error {
+	form, err := c.MultipartForm()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"status":  "error",
+			"message": fmt.Sprintf("Error: Failed to parse multipart form data: %v", err),
+		})
+	}
+	files := form.File["frames"]
+	res, err := sendFramesToMl(files)
+	if err != nil {
+		return c.JSON(http.StatusBadGateway, map[string]string{
+			"status":  "error",
+			"message": fmt.Sprintf("Error: Failed to send file to MMS: %v", err),
+		})
+	}
+	defer func() {
+		err := res.Body.Close()
+		if err != nil {
+			log.Warn().Err(err).Msg("Warning: Failed to close response.")
+		}
+	}()
+	mmsResponse, err := io.ReadAll(res.Body)
+	if err != nil {
+		return c.JSON(http.StatusBadGateway, map[string]string{
+			"status":  "error",
+			"message": fmt.Sprintf("Error: Failed to read MMS response: %v", err),
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]any{
+		"status":       "success",
+		"mms_response": string(mmsResponse),
 	})
 }
 
@@ -43,6 +128,8 @@ func main() {
 
 	r.GET("/", GetIndexHandler)
 	r.GET("/health", GetHealthHandler)
+	r.POST("/:device/attendance/new", PostAttendanceNewHandler)
+
 	err = r.Start(port)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Error: Failed to start server.")
