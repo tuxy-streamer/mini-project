a77c71ea4e817a28fab46ff7ef6d77e8be9bc344
tuxy <subham020304@gmail.com>
2025-07-29 13:18:52 +0530

    feat: add user_id access route

--- cmd/server.go
+++ cmd/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"mime/multipart"
@@ -23,6 +24,11 @@ type FrameMetadata struct {
 	ErrorMsg  string    `json:"error_msg"`
 }
 
+type MMSResponse struct {
+	UserID          int16   `json:"user_id"`
+	ConfidenceScore float32 `json:"confidence_score"`
+}
+
 func GetIndexHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]any{
 		"message": "Welcome to this Mini Project",
@@ -35,6 +41,55 @@ func GetIndexHandler(c echo.Context) error {
 func GetHealthHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
 		"status": "ok",
+	})
+}
+
+func getDataFromML() (*MMSResponse, error) {
+	var (
+		body   bytes.Buffer
+		result MMSResponse
+	)
+	req, err := http.NewRequest("GET", os.Getenv("MMS_FRAME_POST_URL"), &body)
+	if err != nil {
+		return nil, fmt.Errorf("creating request: %w", err)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("sending request: %w", err)
+	}
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			log.Warn().Err(err).Msg("Warning: Failed to close response body")
+		}
+	}()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad status: %d", resp.StatusCode)
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("decoding response: %w", err)
+	}
+	return &result, nil
+}
+
+func GetAttendancePendingHandler(c echo.Context) error {
+	data, err := getDataFromML()
+	if err != nil {
+		return c.JSON(http.StatusBadGateway, map[string]string{
+			"status":  "error",
+			"message": fmt.Sprintf("Error: Failed to read MMS response: %v", err),
+		})
+	}
+	if data.UserID != 0 && data.ConfidenceScore != 0.0 {
+		return c.JSON(http.StatusOK, map[string]any{
+			"status":  "success",
+			"user_id": data.UserID,
+		})
+	}
+	return c.JSON(http.StatusAccepted, map[string]string{
+		"status":  "pending",
+		"message": "UserID not available yet.\n Please try again later.",
 	})
 }
 
@@ -129,6 +184,7 @@ func main() {
 	r.GET("/", GetIndexHandler)
 	r.GET("/health", GetHealthHandler)
 	r.POST("/:device/attendance/new", PostAttendanceNewHandler)
+	r.GET("/:device/attendance/pending", GetAttendancePendingHandler)
 
 	err = r.Start(port)
 	if err != nil {
