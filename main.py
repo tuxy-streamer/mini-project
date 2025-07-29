ca264431bb66f5e963f670341cb4fb92d8fe3050
tuxy <subham020304@gmail.com>
2025-07-29 22:14:03 +0530

    feat: add post route to get images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -14,7 +16,19 @@ class PredictionResponse(BaseModel):
 
 res: PredictionResponse = PredictionResponse(user_id=0, confidence_score=0.0)
 
+
+def user_id_prediction(frames: List[UploadFile]):
+    """Mutate server response with model prediction."""
+
+
 @app.get("/attendance/recognise", response_model=PredictionResponse)
 async def get_attendance_recognise():
     """Return attendance recognition data."""
     return res
+
+
+@app.post("/attendance/recognise")
+async def post_attendance_model(frames: List[UploadFile] = File(...)):
+    """Get the frames for recognition."""
+    user_id_prediction(frames)
+    return {"status": "success"}
